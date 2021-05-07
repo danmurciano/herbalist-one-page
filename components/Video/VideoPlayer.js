edf826a20dd18429088583eb7ea624cdb8267ba1
Dan Murciano <danmurciano@dans-mac-pro.home>
2021-05-06 23:34:19 -0700

    version 1

--- components/Video/VideoPlayer.js
+++ components/Video/VideoPlayer.js
@@ -1,9 +1,9 @@
 export default function VideoIndex({ selectedVideo }) {
   return (
     <iframe
-      width="100%" height="100%"
-      src={selectedVideo.url}
-      frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen">
+      width="100%" height="100%">
+      // src={selectedVideo.url}
+      // frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen">
     </iframe>
   );
 }
