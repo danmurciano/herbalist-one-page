0124361106c6c6a5df83a7e49aad1f868f4aee3c
danmurciano <danm.editing@gmail.com>
2022-07-25 23:57:49 -0700

    version 2

---
+++ next.config.js
@@ -0,0 +1,4 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  future: { webpack5: true }
+};
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,39 @@ import React, {useState, useEffect} from "react";
 import Header from "../components/_App/Header";
 import Main from "../components/Index/Main";
 import MainSmall from "../components/Index/MainSmall";
-import baseUrl from "../utils/baseUrl";
+
 
 export default function Home() {
 
-  const smallScreen = false;
+  const smallScreen = isSmallScreen();
 
+  function isSmallScreen() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      window.addEventListener("resize", handleResize);
+
+      handleResize();
+
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    if (windowSize.width < 840) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 
   return (
